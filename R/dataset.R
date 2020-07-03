4b479992145106fdac3721baf23f74d8f3e4ec78
Akira Hayashi <smxshxishxad@me.com>
2020-07-03 16:59:43 +0900

    :bulb: Add mans

---
+++ R/dataset.R
@@ -0,0 +1,8 @@
+#' The metadata of stock assessment
+#'
+#' @format A list contains fields below:
+#' \describe{
+#'   \item{species}{species name}
+#'   \item{year}{year of the assessment}
+#' }
+"metadata"
