f469a7ddbf5c9b98d76182afa271ec2d824bb358
Akira Hayashi <smxshxishxad@me.com>
2020-07-06 09:33:06 +0900

    :bulb: Update man

--- R/calculate.R
+++ R/calculate.R
@@ -1,6 +1,6 @@
 #' Ultra great function to calculate stock abundance from catch data
 #'
-#' @param catchdata
+#' @param catchdata Catch data
 #' @export
 calculate <- function(catchdata) {
   catchdata * 2
