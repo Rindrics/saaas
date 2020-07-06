96161916abbd2e195dbd9c5124744af16f193930
Akira Hayashi <smxshxishxad@me.com>
2020-07-06 12:51:16 +0900

    :truck: Rename functions

--- R/calculate.R
+++ R/calculate.R
@@ -3,10 +3,31 @@
 #' @param catchdata Catch data
 #' @param coef Mulitiplication coefficient (dummy calculation)
 #' @export
-calculate <- function(catchdata, coef) {
+assess <- function(catchdata, coef) {
   assertthat::assert_that(
     assertthat::has_name(catchdata, c("Year", "Catch"))
   )
   data.frame(Year = catchdata$Year,
-             Abundance = catchdata$Catch * coef)
+             Abundance = catchdata$Catch * coef,
+             Type = "Assessed")
+}
+
+project <- function(abundance, n) {
+  assertthat::assert_that(
+    assertthat::has_name(abundance, c("Year", "Abundance", "Type"))
+  )
+  return_ <- function() {
+    rbind(abundance, projected_())
+  }
+
+  projected_ <- function() {
+    yr_start <- max(abundance$Year) + 1
+    data.frame(Year = yr_start:(yr_start + (n - 1)),
+               Abundance = project_(abundance$Abundance, n),
+               Type = "Projected")
+  }
+  project_ <- function(abund, n) {
+    rep(mean(abund), n)
+  }
+  return_()
 }
--- tests/testthat/test-calculate.R
+++ tests/testthat/test-calculate.R
@@ -1,10 +1,10 @@
-context("behavior of calculate()")
+context("Behavior of calculate()")
 
 test_that("still dummy multiplying df by given value", {
   catch     <- read.csv(system.file("extdata", "catch.csv", package = "saaas"))
-  abundance <- calculate(catch, coef = 2)
+  abundance <- assess(catch, coef = 2)
 
   expect_is(abundance, "data.frame")
-  expect_setequal(colnames(abundance), c("Year", "Abundance"))
+  expect_setequal(colnames(abundance), c("Year", "Abundance", "Type"))
   expect_equal(abundance$Abundance, catch$Catch * 2)
 })
