7fecce56fc63ac78a4785c16268e50bb2b97fb68
Akira Hayashi <smxshxishxad@me.com>
2020-07-06 09:26:57 +0900

    :sparkles: Add calculate()
    Resolve #4

---
+++ R/calculate.R
@@ -0,0 +1,7 @@
+#' Ultra great function to calculate stock abundance from catch data
+#'
+#' @param catchdata
+#' @export
+calculate <- function(catchdata) {
+  catchdata * 2
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Stock assessment as a Softare
