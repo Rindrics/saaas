3b88e756b08f375b96ab233fe2e75d1ec256892b
Akira Hayashi <smxshxishxad@me.com>
2020-07-06 10:38:14 +0900

    :up: Update behavior of calculate()
    Resolve #7

--- tests/testthat/test-calculate.R
+++ tests/testthat/test-calculate.R
@@ -1,7 +1,10 @@
 context("behavior of calculate()")
 
-test_that("still dummy multiplying df by two", {
-  catch <- read.csv(system.file("extdata", "catch.csv", package = "saaas"))
-  expect_is(calculate(catch), "data.frame")
-  expect_equal(calculate(catch), catch * 2)
+test_that("still dummy multiplying df by given value", {
+  catch     <- read.csv(system.file("extdata", "catch.csv", package = "saaas"))
+  abundance <- calculate(catch, coef = 2)
+
+  expect_is(abundance, "data.frame")
+  expect_setequal(colnames(abundance), c("Year", "Abundance"))
+  expect_equal(abundance$Abundance, catch$Catch * 2)
 })
