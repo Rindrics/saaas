2437369bca51ca878f5cc790662f3a64cdddc47f
Akira Hayashi <smxshxishxad@me.com>
2020-07-15 13:50:55 +0900

    :up: Update forecast

--- R/calculate.R
+++ R/calculate.R
@@ -3,30 +3,70 @@
 #' @param catchdata Catch data
 #' @param coef Mulitiplication coefficient (dummy calculation)
 #' @export
-assess <- function(catchdata, coef) {
+assess <- function(catchdata, coef, variable) {
   test_catchdata(catchdata, metadata())
 
-  data.frame(Year = catchdata$Year,
-             Abundance = catchdata$Catch * coef,
-             Type = "Assessed")
+  biomass_ <- function() {
+    value <- catchdata$Catch * coef
+    data.frame(Year = catchdata$Year,
+               Variable = "Biomass",
+               Abundance = value,
+               Type = "Assessed")
+  }
+  ssb_ <- function() {
+    value <- biomass_()$Abundance * 0.6
+    data.frame(Year = catchdata$Year,
+               Variable = "SSB",
+               Abundance = value,
+               Type = "Assessed")
+  }
+  if (variable == "Biomass") {
+    force(biomass_())
+  } else if (variable == "SSB") {
+    force(ssb_())
+  }
 }
 
-project <- function(abundance, n) {
+project <- function(abundance, n, method) {
   assertthat::assert_that(
-    assertthat::has_name(abundance, c("Year", "Abundance", "Type"))
+    assertthat::has_name(abundance, c("Year", "Variable", "Abundance", "Type"))
   )
   return_ <- function() {
-    rbind(abundance, projected_())
+    dplyr::bind_rows(abundance, projected_()) %>%
+      dplyr::mutate(Method = method)
   }
 
   projected_ <- function() {
     yr_start <- max(abundance$Year) + 1
     data.frame(Year = yr_start:(yr_start + (n - 1)),
-               Abundance = project_(abundance$Abundance, n),
+               Variable = unique(abundance$Variable),
+               Abundance = project_(abundance$Abundance, n, var = "mean"),
+               Upper = project_(abundance$Abundance, n, var = "upper"),
+               Lower = project_(abundance$Abundance, n, var = "lower"),
                Type = "Projected")
   }
-  project_ <- function(abund, n) {
-    rep(mean(abund), n)
+  project_ <- function(abund, n, var) {
+    if (method == "arima") {
+      forecast <- abund %>%
+              forecast::auto.arima() %>%
+              forecast::forecast(h = n, level = 80)
+    } else if (method == "randwalk") {
+      forecast <- forecast::rwf(abund, h = n, level = 80, drift = TRUE)
+    }
+      force(unname(forecast[[var]]))
   }
   return_()
+}
+
+assess_and_project <- function(catch) {
+  assess_and_project_ <- function(var, method) {
+    catch %>%
+      assess(coef = 2, variable = var) %>%
+      project(n = 10, method)
+  }
+  vars    <- c("Biomass", "SSB")
+  methods <- c("arima", "randwalk")
+  purrr::pmap_df(list(var = rep(vars, length(methods)),
+                      method = rep(methods, each = length(vars))),
+                 assess_and_project_)
 }
