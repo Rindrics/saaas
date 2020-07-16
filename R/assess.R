a920870ed2f6c9cb2f48eeedd56ea5ef03fd9a21
Akira Hayashi <smxshxishxad@me.com>
2020-07-17 07:44:59 +0900

    :truck: Move block

--- R/assess.R
+++ R/assess.R
@@ -1,13 +1,3 @@
-load_data <- function() {
-  read_ <- function(fname) {
-    utils::read.csv(system.file("extdata", fname, package = "saaas"), row.names = 1)
-  }
-  frasyr::data.handler(caa = read_("catch_at_age.csv"),
-                       waa = read_("weight_at_age.csv"),
-                       maa = read_("maturity_at_age.csv"),
-                       M   = 0.4)
-}
-
 assess <- function(method) {
   result <- load_data() %>%
     calculate(method) %>%
@@ -16,6 +6,16 @@ assess <- function(method) {
   class(result) <- c("assess")
 
   force(result)
+}
+
+load_data <- function() {
+  read_ <- function(fname) {
+    utils::read.csv(system.file("extdata", fname, package = "saaas"), row.names = 1)
+  }
+  frasyr::data.handler(caa = read_("catch_at_age.csv"),
+                       waa = read_("weight_at_age.csv"),
+                       maa = read_("maturity_at_age.csv"),
+                       M   = 0.4)
 }
 
 calculate <- function(dat, method) {
